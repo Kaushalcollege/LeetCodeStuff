ce251b50352712ce069480c4bd684437713294f9
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-09-27 22:18:43 +0530

    Time: 9 ms (100%), Space: 60.4 MB (100%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -1,35 +1,30 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        // Pad costs to make it 1-indexed
+        // Pad the array to make it 1-indexed
         int[] newCosts = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             newCosts[i] = costs[i - 1];
         }
 
-        Integer[] memo = new Integer[n + 1];
-        return dfs(n, newCosts, memo);
-    }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
 
-    private int dfs(int i, int[] costs, Integer[] memo) {
-        if (i == 0) return 0;                 // base case
-        if (i < 0) return Integer.MAX_VALUE;  // invalid state
-        if (memo[i] != null) return memo[i];  // memoized
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
 
-        int best = Integer.MAX_VALUE;
+            // from i-1
+            if (i - 1 >= 0)
+                dp[i] = Math.min(dp[i], dp[i - 1] + newCosts[i] + 1);
 
-        // from i-1
-        if (i - 1 >= 0) {
-            best = Math.min(best, dfs(i - 1, costs, memo) + costs[i] + 1);
-        }
-        // from i-2
-        if (i - 2 >= 0) {
-            best = Math.min(best, dfs(i - 2, costs, memo) + costs[i] + 4);
-        }
-        // from i-3
-        if (i - 3 >= 0) {
-            best = Math.min(best, dfs(i - 3, costs, memo) + costs[i] + 9);
+            // from i-2
+            if (i - 2 >= 0)
+                dp[i] = Math.min(dp[i], dp[i - 2] + newCosts[i] + 4);
+
+            // from i-3
+            if (i - 3 >= 0)
+                dp[i] = Math.min(dp[i], dp[i - 3] + newCosts[i] + 9);
         }
 
-        return memo[i] = best;
+        return dp[n];
     }
 }
