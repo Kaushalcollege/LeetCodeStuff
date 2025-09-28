1549d81f7617efe87217491efc9afe4dbbcb1ebe
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-09-28 19:10:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (91.45%) - LeetHub

---
+++ 3697-compute-decimal-representation/3697-compute-decimal-representation.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def decimalRepresentation(self, n: int) -> List[int]:
+        """
+            -- Non-negetive component means -- from 0 to some large number.
+            -- A positive integer is a base-10 component if it is the product of a single digit from 1 to 9 and a non-negative power of 10.
+            -- For example, 500, 30, and 7 are base-10 components, while 537, 102, and 11 are not.
+            -- we have got to Express n as a sum of only base-10 components, using the fewest base-10 components possible.
+            -- Return an array containing these base-10 components in descending order.
+            -- we can start by breaking down the number digit by digit. (in a loop).
+            -- for every iteration in the loop, multiply with 10 ^ (no of iteration).
+            -- Each non-zero digit contributes a base-10 component.
+        """
+
+        res = []
+        power = 0
+        
+        while n > 0:
+            rem = n % 10
+            if rem != 0:
+                res.append(rem * (10 ** power))
+            n //= 10
+            power += 1
+        
+        return sorted(res, reverse=True)
