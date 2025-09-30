1d86575d7646b8326b8acf3e2ef6645e9289f8c7
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-09-30 07:43:48 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,14 @@
 class Solution {
+    private static Map<Integer,Integer> memo = new HashMap<>();
     public int fib(int n) {
-        if (n <= 1) return n;
-        return fib(n-1) + fib(n-2);
+        if (memo.containsKey(n)) return memo.get(n);
+        int res;
+
+        if (n <= 1) res = n;
+
+        else res = fib(n-1) + fib(n-2);
+
+        memo.put(n, res);
+        return res;
     }
 }
