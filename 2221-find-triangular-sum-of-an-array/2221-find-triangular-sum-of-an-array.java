1ae3b01ebd96d69175cde3827d7cfdb8ac6b5ab5
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-09-30 06:28:59 +0530

    Time: 46 ms (42.77%), Space: 45.7 MB (5.42%) - LeetHub

---
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        /*
+            -- we are initially given an array of length n.
+            -- We are to find out the triangular sum of the array.
+            -- The triangular sum of nums is the value of the only element present in nums after the following process terminates:
+                -- Let nums comprise of n elements. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n - 1.
+                -- For each index i, where 0 <= i < n - 1, assign the value of newNums[i] as (nums[i] + nums[i+1]) % 10, where % denotes modulo operator.
+                -- Replace the array nums with newNums.
+                -- Repeat the entire process starting from step 1.
+                -- return the triangular sum of nums.
+
+                -- the space complexity will be O(n^2) as we are creating a new array newNums.
+
+        */
+
+        int n = nums.length;
+
+        while (n > 1){
+            int[] newNums = new int[n - 1];
+            for (int i = 0; i < n - 1; i++){
+                newNums[i] = (nums[i] + nums[i+1]) % 10;
+            }
+
+            nums = newNums;
+            n--;
+        }
+
+        return nums[0];
+
+    }
+}
