f5c74559789a0f5b7663b34e40dc2d46ad3552e1
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-10-01 13:39:09 +0530

    Time: 26 ms (49.84%), Space: 56.5 MB (90.93%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b); // sort ascending
+
+    for (let i = nums.length - 1; i >= 2; i--) {
+        if (nums[i - 1] + nums[i - 2] > nums[i]) {
+            return nums[i] + nums[i - 1] + nums[i - 2];
+        }
+    }
+
+    return 0;
+};
