64bac10e5a42b97f725d1553375e5c67707c246f
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-10-01 07:43:55 +0530

    Time: 0 ms (100%), Space: 40.5 MB (43.3%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        /*
+            -- Given:
+            -- numBottles are initially full of water.
+            -- we can get i full water bottle if we exchange numExchange empty water bottles.
+            -- the operation of drinking a full water bottle turns it into an empty water bottle.
+            -- RTF:
+            -- The maximum number of water bottle we can drink.
+            -- Constraints:
+            -- 1 <= numBottles <= 100
+            -- 2 <= numExchange <= 100
+
+            -- Initially let empty = numBottles.
+            -- drank = numBottles.
+            -- while (empty > numExchange){
+                newBottles = empty / numExchange
+                empty = empty % numExchange
+
+                drank += newBottles
+                empty = empty + newBottles
+            }
+            -- and then return drank.
+        */
+
+        int empty = numBottles;
+        int drank = numBottles;
+        int newBottles = 0;
+
+        while (empty >= numExchange){
+            newBottles = empty / numExchange;
+            empty %= numExchange;
+            drank += newBottles;
+            empty += newBottles;
+        }
+        
+        return drank;
+    }
+}
