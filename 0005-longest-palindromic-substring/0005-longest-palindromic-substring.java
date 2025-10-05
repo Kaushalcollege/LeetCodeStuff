5123a5042a0cde6a87d224a28e0ae4029ad2bd1f
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-10-05 11:25:18 +0530

    Time: 104 ms (34.5%), Space: 47 MB (6.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,49 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
-        /*
-            -- Brute force is to generate all the possible substrings and then find the longest.
-            -- What is the optimal solution then?
-        */
+        // DP BUTab solution.
 
-    //     int n = s.length();
-    //     String longest = "";
+        if (s.length() <= 1) {
+            return s;
+        }
 
-    //     for (int i = 0; i < n; i++){
-    //         for (int j = i; j < n; j++){
-    //             String subbu = s.substring(i, j + 1);
-    //             if (pal(subbu) & subbu.length() > longest.length()) longest = subbu;
-    //         }
-    //     }
-    //     return longest;
-    // }
+        int maxLen = 1;
+        int start = 0;
+        int end = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
 
-    // private boolean pal (String s){
-    //     int l = 0, r = s.length() - 1;
-    //     while (l <= r){
-    //         if (s.charAt(l) != s.charAt(r)) return false;
-    //         l++;
-    //         r--;
-    //     }
-    //     return true;
-    // }
-
-    int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        String ans = "";
-        
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (len <= 2) dp[i][j] = true;
-                    else dp[i][j] = dp[i+1][j-1];
-                }
-                if (dp[i][j] && len > ans.length()) {
-                    ans = s.substring(i, j + 1);
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
                 }
             }
         }
-        return ans;
-        
+
+        return s.substring(start, end + 1);
     }
 }
