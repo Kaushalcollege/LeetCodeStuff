e328223792eed03c9fa12635b501d2790d4f2902
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-10-05 05:52:51 +0530

    Time: 0 ms (100%), Space: 40.9 MB (5.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static Map<Integer, Integer> memoization = new HashMap<>();
+    public static Map<Integer, Integer> memoization = new HashMap<>(); // for TDMemo.
     public int climbStairs(int n) {
         /*
             -- Most basic implementation of Dynamic Programming.
@@ -10,6 +10,15 @@ class Solution {
 
             -- Top-Down Memoization is tricky.
             -- Declare a static memo data structure.
+            -- Store each n result, when n = 1, 1
+                                         n = 0, 1
+                                         n = 2, 2
+                                         n = 3, 3
+                                         n = 4, 5
+                                         so on so forth.
+            -- Bottom up tabulation is nicer.
+            -- Declare a DP array of size n + 1.
+            -- each index within the array represents the solution to the sub problem of the solution.
         */
 
         // Top-down (Naive)
@@ -21,13 +30,28 @@ class Solution {
 
         // Top-Down memo:-
 
-        if (memoization.containsKey(n)) return memoization.get(n);
-        int res;
+        // if (memoization.containsKey(n)) return memoization.get(n);
+        // int res;
 
-        if (n <= 1) res = 1;
-        else res = climbStairs(n - 1) + climbStairs(n - 2);
+        // if (n <= 1) res = 1;
+        // else res = climbStairs(n - 1) + climbStairs(n - 2);
 
-        memoization.put(n, res);
-        return res;
+        // memoization.put(n, res);
+        // return res;
+
+        // For Bottom - Up Tabulation -- (BUtab)
+
+        if (n <= 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+
     }
 }
