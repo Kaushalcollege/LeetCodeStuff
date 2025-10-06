82cffdb51fb9a6dcd06479ef5bec391df31dd2c6
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-10-06 21:08:00 +0530

    Time: 34 ms (28.76%), Space: 52.4 MB (23.98%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,63 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        Set<List<Integer>> set = new HashSet<>();
-        for (int i = 0; i < nums.length - 1; i++){
-            Set<Integer> ii = new HashSet<>();
-            for (int j = i + 1; j < nums.length; j++){
-                int thirdEle = -1 * (nums[i] + nums[j]);
+        // Set<List<Integer>> set = new HashSet<>();
+        // for (int i = 0; i < nums.length - 1; i++){
+        //     Set<Integer> ii = new HashSet<>();
+        //     for (int j = i + 1; j < nums.length; j++){
+        //         int thirdEle = -1 * (nums[i] + nums[j]);
 
-                if (ii.contains(thirdEle)){
+        //         if (ii.contains(thirdEle)){
+        //             List<Integer> temp = new ArrayList<>();
+        //             temp.add(nums[i]);
+        //             temp.add(nums[j]);
+        //             temp.add(thirdEle);
+        //             Collections.sort(temp);
+        //             set.add(temp);
+        //         }
+        //         ii.add(nums[j]);
+        //     }
+        // }
+        // List<List<Integer>> res = new ArrayList<>();
+        // for (List<Integer> i : set) res.add(i);
+
+        // return res;
+
+        // Optimal Solution:
+
+        Arrays.sort(nums);
+
+        Set<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length - 2; i++){
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = nums.length - 1;
+
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0){
+                    j++;
+                }
+
+                else if (sum > 0){
+                    k--;
+                }
+
+                else {
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
-                    temp.add(thirdEle);
-                    Collections.sort(temp);
+                    temp.add(nums[k]);
                     set.add(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
                 }
-                ii.add(nums[j]);
             }
+
         }
         List<List<Integer>> res = new ArrayList<>();
         for (List<Integer> i : set) res.add(i);
