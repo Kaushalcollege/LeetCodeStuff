ead89006b045138e7c49c4d893e6fb930e8e7327
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2025-11-18 10:22:32 +0530

    Time: 0 ms (100%), Space: 44.2 MB (19.07%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -1,21 +1,37 @@
+// class Solution {
+//     public boolean isOneBitCharacter(int[] bits) {
+//         if (bits.length == 1) return true;
+//         int charStart = -1;
+
+//         int idx = 0;
+//         while (idx < bits.length){
+//             if (bits[idx] == 0) {
+//                 // this is one-bit character
+//                 idx++;
+//                 // continue;
+//             }
+//             else if (bits[idx] == 1 && (bits[idx + 1] == 0 || bits[idx + 1] == 1)){
+//                 charStart = idx;
+//                 idx += 2;
+//             }
+//             if (idx == bits.length - 1 && bits[idx] == 0) return true;
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        if (bits.length == 1) return true;
-        int charStart = -1;
+        int i = 0;
+        int n = bits.length;
 
-        int idx = 0;
-        while (idx < bits.length){
-            if (bits[idx] == 0) {
-                // this is one-bit character
-                idx++;
-                // continue;
-            }
-            else if (bits[idx] == 1 && (bits[idx + 1] == 0 || bits[idx + 1] == 1)){
-                charStart = idx;
-                idx += 2;
-            }
-            if (idx == bits.length - 1 && bits[idx] == 0) return true;
+        while (i < n - 1) { // stop before last bit
+            if (bits[i] == 1) i += 2; // two-bit character
+            else i += 1;              // one-bit character
         }
-        return false;
+
+        return i == n - 1; // true only if last bit stands alone
     }
 }
+
