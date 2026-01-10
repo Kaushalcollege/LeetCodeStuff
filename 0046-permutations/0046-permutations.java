b0cf164190ec4a6271f578e61b74bd11723c7062
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-01-10 14:04:05 +0530

    Time: 2 ms (39.43%), Space: 45.5 MB (49.19%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,37 +1,30 @@
 class Solution {
+    List<Integer> temp = new ArrayList<>();
+    List<List<Integer>> solution = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        /*
-            -- Pattern Recognition:
-            -- The constraints are small as well as the Output format is a list of lists -- screams BackTracking.
-        */
-
-        List<List<Integer>> res = new ArrayList<>();
-        boolean[] vis = new boolean[nums.length];
-
-        backtrackHelper(nums, new ArrayList<>(), vis, res);
-
-        return res;
+        boolean[] considered = new boolean[nums.length];
+        System.out.println(Arrays.toString(considered));
+        f (nums, 0, considered);
+        return solution;
     }
 
-    public void backtrackHelper(int[] nums, List<Integer> path, boolean[] v, List<List<Integer>> res){
-        if (path.size() == nums.length){
-            res.add(new ArrayList<>(path));
-            return; // Backtrack.
+    private void f (int[] arr, int idx, boolean[] considered) {
+        if (idx >= arr.length) {
+            solution.add(new ArrayList<>(temp));
+            return;
         }
 
-        for (int i = 0; i < nums.length; i++){
-            if (v[i]) continue;
+        for (int i = 0; i < arr.length; i++) {
+            if (considered[i]) continue;
 
-            //choose the number:
-            v[i] = true;
-            path.add(nums[i]);
-
-            // Recurse:
-            backtrackHelper(nums, path, v, res);
-
-            // Unchoose or backtrack -- remove the number.
-            path.remove(path.size() - 1);
-            v[i] = false;
+            temp.add(arr[i]);
+            considered[i] = true;
+            f (arr, idx + 1, considered);
+            // temp.remove(temp.size() - 1);
+            // temp.removeAll(temp);
+            considered[i] = false;
+            // f (arr, idx, considered);
+            temp.removeLast();
         }
     }
 }
