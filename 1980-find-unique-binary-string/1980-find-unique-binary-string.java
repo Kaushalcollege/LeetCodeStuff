1de1e0eed88fdc65bd6e8b503f0326532f57319a
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-03-08 16:18:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,28 +1,13 @@
 class Solution {
-    List<String> All = new ArrayList<>();
     public String findDifferentBinaryString(String[] nums) {
-        backTrack (nums[0].length(), 0,  new StringBuilder());
-        // System.out.println(All);
-        Set<String> set = new HashSet<>(Arrays.asList(nums));
 
-        for (String s : All) {
-            if (!set.contains(s)) return s;
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i].charAt(i) == '0') res.append('1');
+            else res.append('0');
         }
-        return "";
-    }
 
-    private void backTrack (int n, int idx, StringBuilder s) {
-
-        if (s.length() == n) {
-            All.add(s.toString());
-            return;
-        }
-        s.append('0');
-        backTrack(n, idx + 1, s);
-        s.deleteCharAt(s.length() - 1);
-        s.append('1');
-        backTrack(n, idx + 1, s);
-        s.deleteCharAt(s.length() - 1);
-        return;
+        return res.toString();
     }
 }
