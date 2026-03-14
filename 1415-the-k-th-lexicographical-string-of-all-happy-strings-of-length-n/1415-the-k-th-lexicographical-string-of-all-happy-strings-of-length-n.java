091e520db650383d15acd816fe6e1508376e918f
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-03-14 20:14:38 +0530

    Time: 18 ms (28.12%), Space: 46.8 MB (36.64%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,34 +1,25 @@
 class Solution {
     ArrayList<String> res = new ArrayList<>();
     public String getHappyString(int n, int k) {
-        f (n, new StringBuilder(), 0);
+        char[] chars = {'a','b','c'};
+        f (n, new StringBuilder(), chars);
         Collections.sort(res);
         if (k > res.size()) return "";
         return res.get(k - 1);
     }
 
-    private void f (int n, StringBuilder sb, int idx) {
+    private void f (int n, StringBuilder sb, char[] chars) {
         if (sb.length() == n) {
-            String s = sb.toString();
-            if (isHappy(s)) res.add(s);
+            res.add(sb.toString());
             return;
         }
-        sb.append('a');
-        f (n, sb, idx + 1);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append('b');
-        f (n, sb, idx + 1);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append('c');
-        f (n, sb, idx + 1);
-        sb.deleteCharAt(sb.length() - 1);
 
-        return;
-    }
+        for (char c : chars) {
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) continue;
 
-    private boolean isHappy (String s) {
-        for (int i = 1; i < s.length(); i++) if (s.charAt(i) == s.charAt(i - 1)) return false;
-
-        return true;
+            sb.append(c);
+            f(n, sb, chars);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
