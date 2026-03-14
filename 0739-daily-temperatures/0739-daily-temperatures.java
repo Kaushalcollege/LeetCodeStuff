cae4474eb4a447550825d2199d1bd4561df35767
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-03-15 00:53:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,20 +5,16 @@ class Solution {
         Stack<int[]> stk = new Stack<>();
 
         for(int i = 0 ; i < n ; i++){
-            if(!stk.isEmpty()){
-                // stk.push(new int[]{temperatures[i], i});
-                // if (stk.peek()[0] < temperatures[i]) {
-                    while (!stk.isEmpty() && stk.peek()[0] < temperatures[i]) {
-                        ans[stk.peek()[1]] = i - stk.peek()[1];
-                        stk.pop();
-                    }
-                    stk.push(new int[]{temperatures[i], i});
-                // }
-                // else stk.push(new int[]{temperatures[i], i});
-            }
-            else{
+            // if(!stk.isEmpty()){
+                while (!stk.isEmpty() && stk.peek()[0] < temperatures[i]) {
+                    ans[stk.peek()[1]] = i - stk.peek()[1];
+                    stk.pop();
+                }
                 stk.push(new int[]{temperatures[i], i});
-            }
+            // }
+            // else{
+            //     stk.push(new int[]{temperatures[i], i});
+            // }
 
             // System.out.println(stk);
         }
