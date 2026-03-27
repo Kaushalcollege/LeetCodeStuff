2829ea3fc00dcb72d9d269ae37c60d3dedc55f77
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-03-27 14:18:58 +0530

    Time: 46 ms (3.2%), Space: 47 MB (64%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        if (k % mat[0].length == 0) return true;
+        // return false;
+        k = k % mat[0].length;
+        System.out.println(k);
+        int[][] ck = new int[mat.length][mat[0].length];
+        for (int i = 0; i < mat.length; i++) {
+            ck[i] = Arrays.copyOf(mat[i], mat[i].length);
+        }
+        // for (int[] row : ck) System.out.println(Arrays.toString(row));
+
+        while (k-- > 0) {
+            for (int i = 0; i < ck.length; i++) shift(ck[i], i);
+            for (int[] row : ck) System.out.println(Arrays.toString(row));
+            // if (Arrays.deepEquals(mat, ck)) return true;
+        }
+
+        // return false;
+        return Arrays.deepEquals(mat, ck);
+    }
+
+    private void shift (int[] arr, int idx) {
+        if (idx % 2 == 0) {
+            int temp = arr[0];
+            for (int i = 0; i < arr.length - 1; i++) arr[i] = arr[i + 1];
+            arr[arr.length - 1] = temp;
+        }
+        else {
+            int temp = arr[arr.length - 1];
+            for (int i = arr.length - 1; i > 0; i--) arr[i] = arr[i - 1];
+            arr[0] = temp;
+        }
+    }
+}
