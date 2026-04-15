1628fde9595f55f61600562349bf3b19cef8836e
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-04-15 23:41:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,25 +1,14 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
-        List<Integer> target_indices = new ArrayList<>();
         int global_min = 101;
 
-        for (int x = 0; x < n; x++) if (words[x].equals(target)) target_indices.add(x);
-
-
-        for (int x : target_indices) {
-            int left_path = 0, right_path = 0;
-            if (x > startIndex) {
-                left_path = n - (x - startIndex);
-                right_path = x - startIndex;
+        for (int x = 0; x < n; x++) {
+            if (words[x].equals(target)) {
+                int diff = ((x - startIndex) > 0) ? x - startIndex : startIndex - x;
+                int dist = (diff < (n - diff)) ? diff : n - diff;
+                global_min = dist < global_min ? dist : global_min;
             }
-
-            else {
-                right_path = n - (startIndex - x);
-                left_path = startIndex - x;
-            }
-
-            global_min = Math.min(global_min, Math.min(left_path, right_path));
         }
 
         return global_min == 101 ? -1 : global_min;
