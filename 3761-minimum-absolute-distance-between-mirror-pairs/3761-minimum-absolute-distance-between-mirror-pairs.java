5ea50b8959bee14388acf09f2294a7a066983a88
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-04-17 21:03:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -1,49 +1,18 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        if (nums.length == 1) return -1;
+        Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-        // System.out.println(rev(12000));
-        // 1 <= nums.length <= 10^5, meaning solution -- O(n) or O(logn)
+        int min = n + 1;
 
-        int min = 100001;
-        // O(n^2)
-        // for (int x = 0; x < n; x++) {
-        //     int ele = nums[x];
-        //     for (int y = x + 1; y < n; y++) {
-        //         // if (ele == rev(nums[y])) min = Math.min(min, y - x);
-        //         if (rev(ele) == nums[y]) min = Math.min(min, y - x);
-        //         else continue;
-        //     }
-        // }
+        for (int x = 0; x < n; x++) {
+            int ele = nums[x];
 
-        // O(n)
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        // map.put(nums[n - 1], new ArrayList<>());
-        // map.get(nums[n - 1]).add(n - 1);
-
-        for (int x = n - 1; x >= 0; x--) {
-            if (map.containsKey(rev(nums[x]))) {
-                int size = map.get(rev(nums[x])).size();
-                int diff = Math.abs(x - map.get(rev(nums[x])).get(size - 1));
-                // min = Math.min(min, Math.abs(x - map.get(rev(nums[x])).get(size - 1)));
-                if (min > diff) min = diff;
-                map.get(rev(nums[x])).add(x);
-                map.put(nums[x], new ArrayList<>());
-                map.get(nums[x]).add(x);
-            }
-            // else if (map.containsKey(nums[x])) {
-
-            // }
-            else {
-                map.put(nums[x], new ArrayList<>());
-                map.get(nums[x]).add(x);
-            }
+            if (map.containsKey(ele)) min = Math.min(min, x - map.get(ele));
+            map.put(rev(ele), x);
         }
 
-        // System.out.println(map);
-        return min == 100001 ? -1 : min;
+        return min == n + 1 ? -1 : min;
     }
-
     private int rev (int x) {
         // Digit Reverse.
         int rev = 0;
