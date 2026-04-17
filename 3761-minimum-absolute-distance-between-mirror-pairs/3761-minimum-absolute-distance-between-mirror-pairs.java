37bf8a5ecd7add8a0fc1c44a358f5e131fb849a1
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-04-17 20:51:55 +0530

    Time: 184 ms (5.58%), Space: 127.5 MB (6.6%) - LeetHub

---
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minMirrorPairDistance(int[] nums) {
+        if (nums.length == 1) return -1;
+        int n = nums.length;
+        // System.out.println(rev(12000));
+        // 1 <= nums.length <= 10^5, meaning solution -- O(n) or O(logn)
+
+        int min = 100001;
+        // O(n^2)
+        // for (int x = 0; x < n; x++) {
+        //     int ele = nums[x];
+        //     for (int y = x + 1; y < n; y++) {
+        //         // if (ele == rev(nums[y])) min = Math.min(min, y - x);
+        //         if (rev(ele) == nums[y]) min = Math.min(min, y - x);
+        //         else continue;
+        //     }
+        // }
+
+        // O(n)
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        // map.put(nums[n - 1], new ArrayList<>());
+        // map.get(nums[n - 1]).add(n - 1);
+
+        for (int x = n - 1; x >= 0; x--) {
+            if (map.containsKey(rev(nums[x]))) {
+                int size = map.get(rev(nums[x])).size();
+                min = Math.min(min, Math.abs(x - map.get(rev(nums[x])).get(size - 1)));
+                map.get(rev(nums[x])).add(x);
+                map.put(nums[x], new ArrayList<>());
+                map.get(nums[x]).add(x);
+            }
+            // else if (map.containsKey(nums[x])) {
+
+            // }
+            else {
+                map.put(nums[x], new ArrayList<>());
+                map.get(nums[x]).add(x);
+            }
+        }
+
+        System.out.println(map);
+        return min == 100001 ? -1 : min;
+    }
+
+    private int rev (int x) {
+        // Digit Reverse.
+        int rev = 0;
+        int rem = 0;
+        while (x > 0) {
+            rem = x % 10;
+            rev = rev * 10 + rem;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
