15f358b066608f93203a6dbd4219616d5070265a
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-04-24 11:02:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -1,36 +1,20 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
-        List<Integer> list = new ArrayList<>();
-        int len = s.length();
+        int n = s.length();
+        int[] res = new int[n];
 
-        for (int x = 0; x < s.length(); x++) if (s.charAt(x) == c) list.add(x);
+        int prev = -n;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == c) prev = i;
+            res[i] = i - prev;
+        }
 
-        int[] res = new int[len];
-        int idx = -1;
-        for (int x = 0; x < len; x++) {
-            idx = binClosest(list, x);
-            res[x] = Math.abs(idx - x);
+        prev = 2 * n;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) prev = i;
+            res[i] = Math.min(res[i], prev - i);
         }
 
         return res;
-    }
-
-    private int binClosest (List<Integer> list, int t) {
-        int n = list.size();
-        int i;
-
-        for (i = 1; i < n; i++) {
-            if (list.get(i) >= t) break;
-        }
-
-        if (i == n) {
-            return list.get(n - 1);
-        }
-      
-        // Check the current and previous element for closest
-        if ((list.get(i) - t) <= t - list.get(i - 1))
-            return list.get(i);
-        else
-            return list.get(i - 1);
     }
 }
