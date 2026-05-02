ea87feccf6f98fcff99fed7e7911d207c2298155
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-02 20:37:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,32 +1,29 @@
 class Solution {
-    public String removeKdigits(String num, int k) {
-        Stack<Character> stack = new Stack<>();
+    public static String removeKdigits(String num, int k) {
+		char[] digits = num.toCharArray();
+		if (k == num.length()) {
+			return "0";
+		}
 
-        for (char digit : num.toCharArray()) {
+		char[] stack = new char[digits.length];
+		int stackTop = -1;
+		int removalCount = k;
 
-            // remove bigger previous digits
-            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
-                stack.pop();
-                k--;
-            }
+		for (int i = 0; i < digits.length; i++) {
+			while (removalCount > 0 && stackTop >= 0 && stack[stackTop] > digits[i]) {
+				stackTop--;
+				removalCount--;
+			}
+			stackTop++;
+			stack[stackTop] = digits[i];
+		}
 
-            stack.push(digit);
-        }
+		int nonZeroStart = 0;
 
-        // if removals still left, remove from end
-        while (k > 0) {
-            stack.pop();
-            k--;
-        }
+		while (stack[nonZeroStart] == '0' && nonZeroStart < digits.length - k - 1) {
+			nonZeroStart++;
+		}
 
-        // build result
-        StringBuilder sb = new StringBuilder();
-        for (char c : stack) sb.append(c);
-
-        // remove leading zeros
-        String result = sb.toString().replaceFirst("^0+(?!$)", "");
-
-        // handle empty case
-        return result.isEmpty() ? "0" : result;
-    }
+		return String.valueOf(stack, nonZeroStart, digits.length - k - nonZeroStart);
+	}	
 }
