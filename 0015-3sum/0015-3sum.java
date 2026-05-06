aafcafcba8afc15b10fd2db656cdb637ccdde87e
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-06 13:21:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //O(n^2)
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
 
+        Arrays.sort(nums);
+
         for (int x = 0; x < n; x++) {
-            Set<Integer> store = new HashSet<>();
-            for (int y = x + 1; y < n; y++) {
-                int z = -(nums[x] + nums[y]);
-                if (store.contains(z)) {
+            if (x > 0 && nums[x] == nums[x - 1]) continue;
+
+            int y = x + 1, z = n - 1;
+
+            while (y < z) {
+                int sum = nums[x] + nums[y] + nums[z];
+
+                if (sum < 0) y++;
+
+                else if (sum > 0) z--;
+
+                else {
                     List<Integer> temp = new ArrayList<>();
-                    Collections.addAll(temp, nums[x], nums[y], z);
-                    Collections.sort(temp);
-                    set.add(temp);
+                    Collections.addAll(temp, nums[x], nums[y], nums[z]);
+                    res.add(temp);
+                    y++;
+                    z--;
+                    while (y < z && nums[y] == nums[y - 1]) y++;
+                    while (y < z && nums[z] == nums[z + 1]) z--;
                 }
-                store.add(nums[y]);
             }
         }
-
-        List<List<Integer>> res = new ArrayList<>();
-        for (List<Integer> l : set) res.add(l);
 
         return res;
     }
