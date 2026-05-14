95a399dff2a400a1efebaae6a864722b006a5826
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-14 17:41:49 +0530

    Time: 0 ms (100%), Space: 47 MB (24.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,36 +6,24 @@ class Solution {
         boolean[][] vis4 = new boolean[m][n];
         int ogColor = image[sr][sc];
 
-        bfs (image, vis4, sr, sc, m, n, newColor, ogColor);
+        dfs (image, vis4, sr, sc, m, n, newColor, ogColor);
 
         return image;
     }
 
-    private void bfs (int[][] img, boolean[][] v, int x, int y, int m, int n, int nC, int og) {
-        Queue<int[]> q = new LinkedList<>();
+    private void dfs (int[][] img, boolean[][] v, int x, int y, int m, int n, int nC, int og) {
+        if (x < 0 || y < 0 || x >= m || y >= n || img[x][y] != og || v[x][y]) return;
 
-        q.add(new int[]{x, y});
         v[x][y] = true;
         img[x][y] = nC;
 
-        int[] dr = {0, 0, -1, 1};
-        int[] dc = {-1, 1, 0, 0};
-
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            int r = curr[0], c = curr[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nc >= 0 && nr >= 0 && nc < n && nr < m && img[nr][nc] == og && !v[nr][nc]) {
-                    v[nr][nc] = true;
-                    img[nr][nc] = nC;
-                    q.add(new int[]{nr, nc});
-                }
-            }
-        }
-
+        // Up.
+        dfs (img, v, x - 1, y, m, n, nC, og);
+        // Down.
+        dfs (img, v, x + 1, y, m, n, nC, og);
+        // right.
+        dfs (img, v, x, y + 1, m, n, nC, og);
+        // left.
+        dfs (img, v, x, y - 1, m, n, nC, og);
     }
 }
