f26222c1d6439bdb8c5e55435affb2a21dace014
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-19 15:25:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,39 +1,34 @@
 class Solution {
-    public void solve(char[][] b) {
-        int m = b.length, n = b[0].length;
+    public void solve(char[][] grid) {
+        int m = grid.length, n = grid3[0].length;
         boolean[][] v = new boolean[m][n];
 
-        for (int x = 0; x < m; x++) {
-            for (int y = 0; y < n; y++) {
-                if (x == 0 || x == m - 1 || y == 0 || y == n - 1) {
-                    if (b[x][y] == 'O' && v[x][y] == false) {
-                        dfs(x, y, v, m, n, b);
-                    }
+        Queue<int[]> q = new LinkedList<>();
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        for (int x = 0; x < m; x++) for (int y = 0; y < n; y++) if (x == 0 || x == m - 1 || y == 0 || y == n - 1) {
+            if (grid[x][y] == 'O') {
+                q.add(new int[]{x, y});
+                v[x][y] = true;
+            }
+        }
+
+        while(!q.isEmpty()) {
+            int[] coordinates = q.poll();
+
+            for (int x = 0; x < 4; x++) {
+                int r = coordinates[0] + dr[x];
+                int c = coordinates[1] + dc[x];
+
+                if (r >= 0 && r < m && c >= 0 && c < n && !v[r][c] && grid[r][c] == 'O') {
+                    q.add(new int[]{r, c});
+                    v[r][c] = true;
                 }
             }
         }
 
-        for (int x = 0; x < m; x++) {
-            for (int y = 0; y < n; y++) {
-                if (b[x][y] == 'O' && v[x][y] == false) b[x][y] = 'X';
-            }
-        }
-
-        // for (boolean[] c : v) {
-        //     System.out.println(Arrays.toString(c));
-        // }
-    }
-
-    private void dfs (int x, int y, boolean[][] v, int m, int n, char[][] b) {
-        if (x >= m || x < 0 || y >= n || y < 0 || v[x][y] == true || b[x][y] != 'O') return;
-
-        v[x][y] = true;
-
-        //Up
-        dfs (x - 1, y, v, m, n, b);
-        dfs (x + 1, y, v, m, n, b);
-        dfs (x, y - 1, v, m, n, b);
-        dfs (x, y + 1, v, m, n, b);
+        for (int x = 0; x < m; x++) for (int y = 0; y < n; y++) if (grid[x][y] == 'O' && !v[x][y]) grid[x][y] = 'X';
 
         return;
     }
