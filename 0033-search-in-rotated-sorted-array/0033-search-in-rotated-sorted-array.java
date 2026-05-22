b29785487ccc724f44f48009fd56bf4bf7c4a996
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-22 22:44:47 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,26 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // The last code was with a time complexity of O(nlogn).
-        // This is with a complexity of O(logn) -- Binary Search.
+        int idx = -1;
+        for (int x = 0; x < nums.length; x++) if (nums[x] == target) idx = x;
 
-        int i = 0, j = nums.length - 1, m = 0;
-
-        while(i <= j){
-            m = i + (j - i) / 2;
-
-            if (nums[m] == target) return m;
-
-            // left subarray (sorted).
-            if (nums[i] <= nums[m]){
-                if (target < nums[m] && target >= nums[i]) j = m - 1;
-                else i = m + 1;
-            }
-
-            else {
-                if (target > nums[m] && target <= nums[j]) i = m + 1;
-                else j = m - 1;
-            }
-        }
-        return -1;
+        return idx;
     }
 }
