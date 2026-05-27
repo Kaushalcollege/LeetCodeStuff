390ae8c2cd0fe60c52b90054a463ba3256f458eb
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-27 15:37:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -17,14 +17,17 @@ class Solution {
 
         for (char c : word.toCharArray()) {
             if (Character.isLowerCase(c) && !set.contains(Character.toUpperCase(c))) set.add(c);
-            else if (Character.isLowerCase(c) && set.contains(Character.toUpperCase(c))) {
+            else if (!set.contains(c) && Character.isLowerCase(c) && set.contains(Character.toUpperCase(c))) {
                 cnt++;
-                set.remove(Character.toUpperCase(c));
+                // set.remove(Character.toUpperCase(c));
+                // System.out.println("1");
             }
             else {
-                if (set.contains(Character.toLowerCase(c))) {
+                 if (!set.contains(c) && set.contains(Character.toLowerCase(c))) {
                     cnt++;
-                    set.remove(Character.toLowerCase(c));
+                    set.add(c);
+                    // System.out.println("2");
+                    // set.remove(Character.toLowerCase(c));
                 }
                 // else if (set.isEmpty()) set.add(c);
                 else set.add(c);
