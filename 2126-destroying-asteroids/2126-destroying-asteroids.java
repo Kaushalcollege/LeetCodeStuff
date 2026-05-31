e91b5e691c5f1af841d5b6c5117e9a0e57e28c28
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-05-31 22:27:07 +0530

    Time: 5 ms (99.54%), Space: 105.9 MB (92.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,11 +1,39 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+
+        // // this is a greedy approach.
+        // long m = mass;
+        // Arrays.sort(asteroids); // O(nlogn)
+
+        // for (int x : asteroids) { // O(n)
+        //     if (m >= x) m += x;
+        //     else return false;
+        // }
+
+        // return true; // finally O(nlogn)
+
+        // How do I get it to O(n)?
+
+        int min = 100001;
+        int max = 0;
+
+        for (int x : asteroids) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+
+        int[] freq = new int[max + 1];
         long m = mass;
-        Arrays.sort(asteroids);
 
         for (int x : asteroids) {
             if (m >= x) m += x;
-            else return false;
+            else freq[x]++;
+        }
+
+        if (m >= max) return true;
+        for (int x = min; x <= max; x++) {
+            if (m < x) return false;
+            if (freq[x] != 0) m += (x * freq[x]);
         }
 
         return true;
