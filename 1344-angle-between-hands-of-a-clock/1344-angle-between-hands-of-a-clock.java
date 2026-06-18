86a6037f93ba4d73b3390320d0eb7be1b3726ab9
Y. Kaushal Mohan Kumar <147945999+Kaushalcollege@users.noreply.github.com>
2026-06-18 13:25:15 +0530

    Time: 0 ms (100%), Space: 44.9 MB (60.07%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -1,37 +1,14 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
-        /*
-            -- Microsoft SDE Intern PYQ.
-            -- What should be done exactly?
-            -- Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the hour and the minute hand.
+        // At 12 we are to assume that is 0 deg angle.
 
-            -- Answers within 10^5 of the actual value will be accepted as correct.
-            -- The fact is that, it is really simple.
-            -- To put it into words, suppose the time is 12 : 30.
-            -- The minute hand is at 6 and hour hand is exactly in the middle of 12 and 1.
-            -- If we assume the 360 / 12 == 30 deg.
-            -- Since the hour hand is exactly between 12 and 1 it has already covered 15 deg from origin.
-            -- And minute hand has covered 180 deg. so angle = Math.abs(cover(min) - cover(hour)).
-            -- To put that into code, we can create a covered() function which will return the degress covered by both. or no need.
-            -- to execute this we have to structure our code in a neat manner.
+        if (hour == 12) hour = 0;
+        double ratio = (double)minutes / 60;
+        // System.out.println(ratio);
+        double actualHour = 30 * (ratio + hour);
+        // System.out.println(actualHour);
+        double res = Math.abs((6 * minutes) - actualHour);
 
-        */
-        double hA = hourCovered(hour);
-        if (minCovered(minutes) > 0){
-            hA += (minCovered(minutes) / 360) * 30;
-        }
-
-        if (Math.abs(minCovered(minutes) - hA) > 180.0) return 360 - Math.abs(minCovered(minutes) - hA);
-
-        return Math.abs(minCovered(minutes) - hA);
-    }
-
-    private double minCovered (int m){
-        return ((double)m / 60) * 360;
-    }
-
-    private double hourCovered (int h){
-        if (((double)h / 12) * 360 == 360) return 0;
-        return ((double)h / 12) * 360;
+        return res <= 180 ? res : 360 - res;
     }
 }
